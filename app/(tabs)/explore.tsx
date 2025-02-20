f1a75e612a8cb4cdf73ee099625d8b829316c9a2
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-02-20 23:00:21 +0530

    fix: bottom sheet broken layout

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -7,57 +7,76 @@ import { Link } from "expo-router";
 import { useState } from "react";
 import { FlatList, Image, StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function () {
-  const [pictureOpen, setPictureOpen] = useState(false);
+  const [selectedWallpaper, setSelectedWallpaper] = useState<null | Wallpaper>(
+    null,
+  );
   const wallpaper = useWallpaper();
+
   return (
-    <SafeAreaView
-      style={{
-        flex: 1,
-        backgroundColor: "brown",
-      }}
-    >
-      <ParallaxScrollView
-        headerBackgroundColor={{ dark: "black", light: "white" }}
-        headerImage={
-          <Image
-            style={{ flex: 1 }}
-            source={{
-              uri: "https://images.pexels.com/photos/7672255/pexels-photo-7672255.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-            }}
-          />
-        }
-      >
-        <ThemedView style={styles.container}>
-          <ThemedView style={styles.innerContainer}>
-            <FlatList
-              data={wallpaper.filter((_, index) => index % 2 === 0)}
-              renderItem={({ item }) => (
-                <View style={styles.imageContainer}>
-                  <ImageCard wallpaper={item} setPictureOpen={setPictureOpen} />
-                </View>
-              )}
-              keyExtractor={(item) => item.name}
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaView style={{ flex: 1, backgroundColor: "brown" }}>
+        <View style={{ flex: 1 }}>
+          <ParallaxScrollView
+            headerBackgroundColor={{ dark: "black", light: "white" }}
+            headerImage={
+              <Image
+                style={{ flex: 1 }}
+                source={{
+                  uri: "https://images.pexels.com/photos/7672255/pexels-photo-7672255.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+                }}
+              />
+            }
+            // scrollEnabled={!selectedWallpaper}
+          >
+            <ThemedView style={styles.container}>
+              <ThemedView style={styles.innerContainer}>
+                <FlatList
+                  data={wallpaper.filter((_, index) => index % 2 === 0)}
+                  renderItem={({ item }) => (
+                    <View style={styles.imageContainer}>
+                      <ImageCard
+                        wallpaper={item}
+                        onPress={() => {
+                          setSelectedWallpaper(item);
+                        }}
+                      />
+                    </View>
+                  )}
+                  keyExtractor={(item) => item.name}
+                  scrollEnabled={!selectedWallpaper}
+                />
+              </ThemedView>
+              <ThemedView style={styles.innerContainer}>
+                <FlatList
+                  data={wallpaper.filter((_, index) => index % 2 === 1)}
+                  renderItem={({ item }) => (
+                    <View style={styles.imageContainer}>
+                      <ImageCard
+                        wallpaper={item}
+                        onPress={() => {
+                          setSelectedWallpaper(item);
+                        }}
+                      />
+                    </View>
+                  )}
+                  keyExtractor={(item) => item.name}
+                  scrollEnabled={!selectedWallpaper}
+                />
+              </ThemedView>
+            </ThemedView>
+          </ParallaxScrollView>
+          {selectedWallpaper && (
+            <DownloadPicture
+              onClose={() => setSelectedWallpaper(null)}
+              wallpaper={selectedWallpaper}
             />
-          </ThemedView>
-          <ThemedView style={styles.innerContainer}>
-            <FlatList
-              data={wallpaper.filter((_, index) => index % 2 === 1)}
-              renderItem={({ item }) => (
-                <View style={styles.imageContainer}>
-                  <ImageCard wallpaper={item} setPictureOpen={setPictureOpen} />
-                </View>
-              )}
-              keyExtractor={(item) => item.name}
-            />
-          </ThemedView>
-        </ThemedView>
-        {pictureOpen && (
-          <DownloadPicture onClose={() => setPictureOpen(false)} />
-        )}
-      </ParallaxScrollView>
-    </SafeAreaView>
+          )}
+        </View>
+      </SafeAreaView>
+    </GestureHandlerRootView>
   );
 }
 
@@ -75,22 +94,3 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
   },
 });
-
-// <View
-//         style={{
-//           flex: 1,
-//           backgroundColor: "gold",
-//         }}
-//       >
-//         <Text>Accounts</Text>
-//         <Button
-//           title="Open Bottom Sheet"
-//           onPress={() => {
-//             setPictureOpen(true);
-//           }}
-//         ></Button>
-//
-//         {pictureOpen && (
-//           <DownloadPicture onClose={() => setPictureOpen(false)} />
-//         )}
-//       </View>
