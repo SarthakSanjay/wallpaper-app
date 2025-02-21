84445ea48cfa11cc89de13fa126c769b52cbe213
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-02-21 23:51:37 +0530

    refactor: turned view into themedView

--- components/BottomSheet.tsx
+++ components/BottomSheet.tsx
@@ -16,6 +16,8 @@ import { Ionicons } from "@expo/vector-icons";
 import { Colors } from "@/constants/Colors";
 import * as MediaLibrary from "expo-media-library";
 import * as FileSystem from "expo-file-system";
+import { ThemedView } from "./ThemedView";
+import { ThemedText } from "./ThemedText";
 
 const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
@@ -53,74 +55,99 @@ export const DownloadPicture = ({
       android_keyboardInputMode="adjustPan"
     >
       <BottomSheetView style={styles.contentContainer}>
-        <Image
-          source={{ uri: wallpaper.url }}
-          style={styles.image}
-          // resizeMode="contain"
-        />
-        <View style={styles.topbar}>
-          <Pressable onPress={onClose}>
-            <Ionicons
-              name={"close"}
-              size={24}
-              color={theme === "light" ? Colors.light.icon : Colors.dark.icon}
-            />
-          </Pressable>
-
-          <View
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "center",
-              gap: 5,
-            }}
-          >
-            <Ionicons
-              name={"heart"}
-              size={24}
-              color={theme === "light" ? Colors.light.icon : Colors.dark.icon}
-            />
-            <Ionicons
-              name={"share-social-outline"}
-              size={24}
-              color={theme === "light" ? Colors.light.icon : Colors.dark.icon}
-            />
-          </View>
-        </View>
-        <View style={styles.titleContainer}>
-          <Text
-            style={{
-              color: "white",
-              fontSize: 24,
-              fontWeight: 500,
-              fontStyle: "italic",
-            }}
-          >
-            {wallpaper.name}
-          </Text>
-        </View>
-        <Pressable
-          style={styles.button}
-          onPress={async () => {
-            let date = new Date().getTime();
-            let fileUri = FileSystem.documentDirectory + `${date}.jpg`;
-
-            try {
-              await FileSystem.downloadAsync(wallpaper.url, fileUri);
-              const response = await MediaLibrary.requestPermissionsAsync(true);
-              if (response.granted) {
-                MediaLibrary.createAssetAsync(fileUri);
-                alert("Image saved");
-              } else {
-                console.error("permission not granted");
-              }
-            } catch (err) {
-              console.log("FS Err: ", err);
-            }
+        <ThemedView
+          style={{
+            flex: 1,
+            borderTopLeftRadius: 15,
+            borderTopRightRadius: 15,
           }}
         >
-          <Button title="Download" color={"transparent"}></Button>
-        </Pressable>
+          <Image
+            source={{ uri: wallpaper.url }}
+            style={styles.image}
+            // resizeMode="contain"
+          />
+          <View style={styles.topbar}>
+            <Pressable onPress={onClose}>
+              <Ionicons
+                name={"close"}
+                size={24}
+                color={theme === "light" ? Colors.light.icon : Colors.dark.icon}
+              />
+            </Pressable>
+
+            <ThemedView
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+                gap: 5,
+                backgroundColor: "transparent",
+              }}
+            >
+              <Ionicons
+                name={"heart"}
+                size={24}
+                color={theme === "dark" ? Colors.dark.icon : Colors.light.icon}
+              />
+              <Ionicons
+                name={"share-social-outline"}
+                size={24}
+                color={theme === "dark" ? Colors.dark.icon : Colors.light.icon}
+              />
+            </ThemedView>
+          </View>
+          <ThemedView style={styles.titleContainer}>
+            <ThemedText
+              style={{
+                fontSize: 24,
+                fontWeight: 500,
+                fontStyle: "italic",
+              }}
+            >
+              {wallpaper.name}
+            </ThemedText>
+          </ThemedView>
+          <Pressable
+            // style={styles.button}
+            style={({ pressed }) => ({
+              backgroundColor:
+                theme === "dark"
+                  ? Colors.light.background
+                  : Colors.dark.background,
+              ...styles.button, // Spread the button styles after the dynamic styles
+            })}
+            onPress={async () => {
+              let date = new Date().getTime();
+              let fileUri = FileSystem.documentDirectory + `${date}.jpg`;
+
+              try {
+                await FileSystem.downloadAsync(wallpaper.url, fileUri);
+                const response =
+                  await MediaLibrary.requestPermissionsAsync(true);
+                if (response.granted) {
+                  MediaLibrary.createAssetAsync(fileUri);
+                  alert("Image saved");
+                } else {
+                  console.error("permission not granted");
+                }
+              } catch (err) {
+                console.log("FS Err: ", err);
+              }
+            }}
+          >
+            {({ pressed }) => (
+              <ThemedText
+                style={{
+                  color:
+                    theme === "dark" ? Colors.light.text : Colors.dark.text,
+                }}
+              >
+                Download
+              </ThemedText>
+            )}
+          </Pressable>
+        </ThemedView>
       </BottomSheetView>
     </BottomSheet>
   );
@@ -138,10 +165,10 @@ const styles = StyleSheet.create({
   },
   contentContainer: {
     flex: 1,
-    backgroundColor: "#272829",
     paddingInline: 0,
     borderTopLeftRadius: 20,
     borderTopRightRadius: 20,
+    backgroundColor: "transparent",
   },
   image: {
     height: "60%",
@@ -154,7 +181,7 @@ const styles = StyleSheet.create({
     width: "50%",
     marginInline: "auto",
     marginTop: 10,
-    backgroundColor: "black",
+    // backgroundColor: "black",
     borderRadius: 10,
     height: 40,
     display: "flex",
@@ -163,8 +190,8 @@ const styles = StyleSheet.create({
   },
   topbar: {
     position: "absolute",
+    backgroundColor: "transparent",
     width: "100%",
-    // backgroundColor: "transparent",
     padding: 10,
     display: "flex",
     flexDirection: "row",
