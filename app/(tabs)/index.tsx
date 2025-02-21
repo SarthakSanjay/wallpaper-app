7f2861ad382b67f2b6ebf6dc7ef0b9976baa1af9
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-02-21 23:52:31 +0530

    add: library ,liked & suggested component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,12 @@
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
-import Suggested from "../suggested";
-import Library from "../library";
-import Liked from "../liked";
+import { ThemedView } from "@/components/ThemedView";
+import { SplitView } from "@/components/SplitView";
+import {
+  useLibraryWallpaper,
+  useLikedWallpaper,
+  useSuggestedWallpaper,
+  useWallpaper,
+} from "@/hooks/useWallpapers";
 
 const Tab = createMaterialTopTabNavigator();
 
@@ -12,5 +17,48 @@ export default function Foryou() {
       <Tab.Screen name="Liked" component={Liked}></Tab.Screen>
       <Tab.Screen name="Suggested" component={Suggested}></Tab.Screen>
     </Tab.Navigator>
+    // <ThemedSafeAreaView style={styles.container}>
+    // </ThemedSafeAreaView>
+  );
+}
+
+function Library() {
+  const wallpaper = useLibraryWallpaper();
+
+  return (
+    <ThemedView
+      style={{
+        flex: 1,
+      }}
+    >
+      <SplitView wallpaper={wallpaper} />
+    </ThemedView>
+  );
+}
+
+function Liked() {
+  const wallpaper = useLikedWallpaper();
+
+  return (
+    <ThemedView
+      style={{
+        flex: 1,
+      }}
+    >
+      <SplitView wallpaper={wallpaper} />
+    </ThemedView>
+  );
+}
+
+function Suggested() {
+  const wallpaper = useSuggestedWallpaper();
+  return (
+    <ThemedView
+      style={{
+        flex: 1,
+      }}
+    >
+      <SplitView wallpaper={wallpaper} />
+    </ThemedView>
   );
 }
