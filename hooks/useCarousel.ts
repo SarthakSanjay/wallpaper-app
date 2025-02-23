c489c1af673be0fda2115ae53424a51f10031e94
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-02-23 23:55:10 +0530

    create: useCarousel hook

---
+++ hooks/useCarousel.ts
@@ -0,0 +1,10 @@
+export default function useCarousel() {
+  return [
+    "https://images.pexels.com/photos/33045/lion-wild-africa-african.jpg?auto=compress&cs=tinysrgb&w=1200",
+    "https://images.pexels.com/photos/60597/dahlia-red-blossom-bloom-60597.jpeg?auto=compress&cs=tinysrgb&w=1200",
+    "https://images.pexels.com/photos/66997/pexels-photo-66997.jpeg?auto=compress&cs=tinysrgb&w=1200",
+    "https://images.pexels.com/photos/2049422/pexels-photo-2049422.jpeg?auto=compress&cs=tinysrgb&w=1200",
+    "https://images.pexels.com/photos/325185/pexels-photo-325185.jpeg?auto=compress&cs=tinysrgb&w=1200",
+    "https://images.pexels.com/photos/1042423/pexels-photo-1042423.jpeg?auto=compress&cs=tinysrgb&w=1200",
+  ];
+}
