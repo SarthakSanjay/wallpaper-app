c9669b1f0fd2c79027db58953820b4d05eafd1eb
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-02-23 23:53:02 +0530

    add: Image carousel

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -3,11 +3,11 @@ import ImageCard from "@/components/ImageCard";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedView } from "@/components/ThemedView";
 import { useWallpaper, Wallpaper } from "@/hooks/useWallpapers";
-import { Link } from "expo-router";
 import { useState } from "react";
-import { FlatList, Image, StyleSheet, View } from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import ImageCarousel from "@/components/ImageCarousel";
 
 export default function () {
   const [selectedWallpaper, setSelectedWallpaper] = useState<null | Wallpaper>(
@@ -21,15 +21,7 @@ export default function () {
         <View style={{ flex: 1 }}>
           <ParallaxScrollView
             headerBackgroundColor={{ dark: "black", light: "white" }}
-            headerImage={
-              <Image
-                style={{ flex: 1 }}
-                source={{
-                  uri: "https://images.pexels.com/photos/7672255/pexels-photo-7672255.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-                }}
-              />
-            }
-            // scrollEnabled={!selectedWallpaper}
+            headerImage={<ImageCarousel />}
           >
             <ThemedView style={styles.container}>
               <ThemedView style={styles.innerContainer}>
